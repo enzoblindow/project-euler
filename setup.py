2b489b047c8c34092f541df1f4c3739359ffdcda
Enzo Blindow <enzo@blindow.eu>
2017-07-08 17:38:34 +0200

    euler module into package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='euler',
+    version='0.0.1',
+    packages=['euler'],
+    description='Setup Assistant for personal solution library '
+                'for project euler problems ',
+    url='',
+    author='Enzo',
+    author_email='',
+    license='',
+)
