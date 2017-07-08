052b7b977ddac6f890c2fd71fc561642f822c9ec
Enzo Blindow <enzo@blindow.eu>
2017-07-08 17:14:41 +0200

    cleanup

--- p001/__init__.py
+++ p001/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from project_euler.euler import euler
+
 MULTIPLES = [3, 5]
 
 def is_multiple(num):
@@ -9,10 +11,9 @@ def is_multiple(num):
             return True
     return False
 
+@euler(pid=1, update_readme=True)
 def solve(num):
     return sum([i for i in range(num) if is_multiple(i)])
 
 if __name__ == '__main__':
-    answer = solve(1000)
-    print 'Problem: Find the sum of all the multiples of 3 or 5 below 1000.'
-    print 'Solution: {}'.format(answer)
+    solve(1000)
