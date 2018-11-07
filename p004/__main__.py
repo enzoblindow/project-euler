df3d1374247b3d2d746eb5e9743cca0ee531ad52
Enzo Blindow <enzo@blindow.eu>
2018-11-07 18:29:20 +0100

    solve p004

--- p004/__main__.py
+++ p004/__main__.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from project_euler.euler import euler
+from euler import euler
 
 
-@euler(pid=4, update_readme=False)
+def is_palindrome(number):
+    number = str(number)
+    a = number[:3]
+    b = number[3:]
+
+    return a == b[::-1]
+
+
+@euler(pid=4, update_readme=True)
 def solve():
-    return 'WIP'
+    firsts = range(100, 1000)[::-1]
+    seconds = range(100, 1000)[::-1]
+    for first in firsts:
+        for second in seconds:
+            number = first * second
+            if is_palindrome(number):
+                return first, second, number
 
 
 if __name__ == '__main__':
-    solve()
+    print solve()
 
     
