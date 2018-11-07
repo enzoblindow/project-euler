6416e6302af3bd003d5f893a796c27cfb5e580be
Enzo Blindow <enzo@blindow.eu>
2018-11-07 18:15:45 +0100

    new p003

---
+++ p003/__main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from project_euler.euler import euler
+
+
+@euler(pid=3, update_readme=False)
+def solve():
+    return 'WIP'
+
+
+if __name__ == '__main__':
+    solve()
+
+    
