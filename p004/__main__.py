bbe3e280455fc5e44943756df45f62aa3e02f105
Enzo Blindow <enzo@blindow.eu>
2018-11-10 15:19:49 +0100

    speed up p004

--- p004/__main__.py
+++ p004/__main__.py
@@ -3,30 +3,30 @@
 from euler import euler
 
 
-def is_palindrome(number):
-    number = str(number)
-    a = number[:3]
-    b = number[3:]
-
-    return a == b[::-1]
+def is_palindrome(num):
+    return str(num)[:3] == str(num)[3:][::-1]
 
 
 @euler(pid=4, update_readme=True)
 def solve():
-    results = list()
+    max = 0
     firsts = range(100, 1000)[::-1]
     seconds = range(100, 1000)[::-1]
 
     for first in firsts:
         for second in seconds:
             number = first * second
-            if is_palindrome(number):
-                results.append(number)
 
-    return max(results)
+            if number < max:
+                break
+
+            if is_palindrome(number):
+                if number > max:
+                    max = number
+                    break
+
+    return max
 
 
 if __name__ == '__main__':
     print solve()
-
-    
