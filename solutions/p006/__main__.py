0a4eae9f670c66c1482221db3611095f431bef8b
Enzo Blindow <16123902+enzoblindow@users.noreply.github.com>
2023-12-11 14:10:26 +0000

    solve p006

--- solutions/p006/__main__.py
+++ solutions/p006/__main__.py
@@ -3,12 +3,15 @@
 from euler import euler
 
 
-@euler(pid=006, update_readme=False)
+@euler(pid=6, update_readme=False)
 def solve():
-    return 'WIP'
+    sum_of_squares = sum(i**2 for i in range(1, 101))
+    sum_squared = sum(range(1, 101))**2
+
+    return sum_squared - sum_of_squares
 
 
 if __name__ == '__main__':
-    print solve()
+    print(solve())
 
     
